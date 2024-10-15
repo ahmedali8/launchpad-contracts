b8a680a617ccc64d82ff2d42f0edde7bf20e028f
Ahmed Ali <imahmedalibhatti@gmail.com>
2024-10-15 05:54:23 +0100

    fix: remove the unnecessary conversion prop

--- contracts/types/TEscrow.sol
+++ contracts/types/TEscrow.sol
@@ -13,25 +13,19 @@ library TEscrow {
     }
 
     /// @notice Struct to hold user details for the Escrow contract.
-    /// @dev The struct is byte-packed to optimize storage.
-    /// Each `uint256` variable occupies 32 bytes, and `OptStatus` occupies 1 byte.
-    /// The total size of this struct is 129 bytes, rounded up to 160 bytes due to Solidity's 32-byte storage slot
-    /// alignment.
     ///
-    /// @dev Packed into 5 storage slots (129 bytes)
+    /// @dev Packed into 4 storage slots
+    /// Total size: 97 bytes (rounded up to 128 bytes due to storage slot alignment).
+    /// TODO: usdcBalance and dynUSDCBalance can be packed in one int256 variable to save a slot or yieldBalance and
+    /// optStatus in one ??
     /// @param usdcBalance The balance of USDC held by the user in the Escrow contract. (32 bytes)
     /// @param dynUSDCBalance The balance of DynUSDC held by the user if they opted into yield earning. (32 bytes)
     /// @param yieldBalance The accumulated yield earned by the user through Dynavault. (32 bytes)
-    /// @param conversionRatio The ratio used to convert USDC to DynUSDC, in basis points (e.g., 1 USDC = 1.02 DynUSDC).
-    /// (32 bytes)
     /// @param optStatus The current opt-in status of the user, whether they are opted in or out. (1 byte)
-    ///
-    /// @dev Total size: 129 bytes (rounded up to 160 bytes due to storage slot alignment).
     struct UserInfo {
         uint256 usdcBalance;
         uint256 dynUSDCBalance;
         uint256 yieldBalance;
-        uint256 conversionRatio;
         OptStatus optStatus;
     }
 }
