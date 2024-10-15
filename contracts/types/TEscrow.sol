24df429c80f1660577c84e8ae01f7091ab7f55c2
Ahmed Ali <imahmedalibhatti@gmail.com>
2024-10-15 08:51:53 +0100

    feat: add  prop

--- contracts/types/TEscrow.sol
+++ contracts/types/TEscrow.sol
@@ -15,17 +15,20 @@ library TEscrow {
     /// @notice Struct to hold user details for the Escrow contract.
     ///
     /// @dev Packed into 4 storage slots
-    /// Total size: 97 bytes (rounded up to 128 bytes due to storage slot alignment).
+    /// Total size: 98 bytes (rounded up to 128 bytes due to storage slot alignment).
     /// TODO: usdcBalance and dynUSDCBalance can be packed in one int256 variable to save a slot or yieldBalance and
     /// optStatus in one ??
     /// @param usdcBalance The balance of USDC held by the user in the Escrow contract. (32 bytes)
     /// @param dynUSDCBalance The balance of DynUSDC held by the user if they opted into yield earning. (32 bytes)
     /// @param yieldBalance The accumulated yield earned by the user through Dynavault. (32 bytes)
+    /// @param hasParticipated A flag to indicate whether the user has deposited his USDC or DynUSDC into the
+    /// `ProjectAllocationManager` contract. (1 byte)
     /// @param optStatus The current opt-in status of the user, whether they are opted in or out. (1 byte)
     struct UserInfo {
         uint256 usdcBalance;
         uint256 dynUSDCBalance;
         uint256 yieldBalance;
+        bool hasParticipated;
         OptStatus optStatus;
     }
 }
