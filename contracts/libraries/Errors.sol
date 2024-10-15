e1e3a66e8df9e4848d4bc3029626a2959c4268eb
Ahmed Ali <imahmedalibhatti@gmail.com>
2024-10-15 05:33:36 +0100

    feat: add common erros library

---
+++ contracts/libraries/Errors.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.22;
+
+library Errors {
+    /// @notice TimelineInfo Struct: Error for when the project start time is not in the future.
+    /// @param currentTimestamp The current block timestamp.
+    /// @param startTime The project start time.
+    error LaunchpadV3_TimelineInfo_InvalidStartTime(uint40 currentTimestamp, uint40 startTime);
+
+    /// @notice TimelineInfo Struct: Error for when the project end time is not after the start time.
+    /// @param startTime The project start time.
+    /// @param endTime The project end time.
+    error LaunchpadV3_TimelineInfo_InvalidEndTime(uint40 startTime, uint40 endTime);
+
+    /// @notice Thrown when the start time of vesting is greater than or equal to the cliff end time.
+    /// @param vestingStartTime The timestamp when vesting starts.
+    /// @param vestingCliffEndTime The timestamp when the cliff period ends.
+    error LaunchpadV3_VestingSchedule_InvalidCliffTime(uint40 vestingStartTime, uint40 vestingCliffEndTime);
+
+    /// @notice Thrown when the cliff end time is greater than the vesting end time.
+    /// @param vestingCliffEndTime The timestamp when the cliff period ends.
+    /// @param vestingEndTime The timestamp when vesting ends.
+    error LaunchpadV3_VestingSchedule_InvalidEndTime(uint40 vestingCliffEndTime, uint40 vestingEndTime);
+}
