dad1eef45829f525408d775fc9b5758cd1f5c4d0
Ahmed Ali <imahmedalibhatti@gmail.com>
2024-10-17 08:44:49 +0100

    feat: change decimals to 6 for usdc

--- contracts/mocks/USDCMock.sol
+++ contracts/mocks/USDCMock.sol
@@ -7,6 +7,11 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract USDCMock is ERC20 {
     constructor() ERC20("USDCMock", "USDCM") { }
 
+    // solhint-disable-next-line
+    function decimals() public view override returns (uint8) {
+        return 6;
+    }
+
     function mint(address account, uint256 amount) external {
         _mint(account, amount);
     }
