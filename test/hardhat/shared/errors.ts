182278f0d73323402ca379925134b7bb650dd647
Ahmed Ali <imahmedalibhatti@gmail.com>
2024-10-17 08:50:20 +0100

    test: add errors for hardhat tests

---
+++ test/hardhat/shared/errors.ts
@@ -0,0 +1,15 @@
+export enum Errors {
+  // Generics
+  LaunchpadV3_UserAlreadyOptedIn = "LaunchpadV3_UserAlreadyOptedIn",
+  LaunchpadV3_UserNotOptedIn = "LaunchpadV3_UserNotOptedIn",
+  // AddressLibrary
+  LaunchpadV3_AddressLibrary_InvalidAddress = "LaunchpadV3_AddressLibrary_InvalidAddress",
+  // Escrow
+  LaunchpadV3_Escrow_AlreadyInitialized = "LaunchpadV3_Escrow_AlreadyInitialized",
+  LaunchpadV3_Escrow_InvalidAmount = "LaunchpadV3_Escrow_InvalidAmount",
+  LaunchpadV3_Escrow_InsufficientUSDCBalance = "LaunchpadV3_Escrow_InsufficientUSDCBalance",
+  LaunchpadV3_Escrow_InsufficientDynUSDCBalance = "LaunchpadV3_Escrow_InsufficientDynUSDCBalance",
+  LaunchpadV3_Escrow_UserCannotWithdraw = "LaunchpadV3_Escrow_UserCannotWithdraw",
+  // AccessControl
+  AccessControlUnauthorizedAccount = "AccessControlUnauthorizedAccount", // AccessControlUnauthorizedAccount(address,bytes32);
+}
