cca7e12559eeb555c07604e0f04d55f495be62e2
Ahmed Ali <imahmedalibhatti@gmail.com>
2024-10-21 18:48:08 +0100

    feat: update the design from SR-2010

--- contracts/interfaces/IEscrow.sol
+++ contracts/interfaces/IEscrow.sol
@@ -2,7 +2,63 @@
 
 pragma solidity ^0.8.22;
 
+// types
+import { TCommon } from "../types/TCommon.sol";
+
 interface IEscrow {
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Event emitted when a user deposits USDC.
+    /// @param user The address of the user who deposited USDC.
+    /// @param amount The amount of USDC deposited by the user.
+    /// @param optStatus The opt-in status of the user for the deposit.
+    event Deposited(address indexed user, uint256 amount, TCommon.OptStatus optStatus);
+
+    /// @notice Event emitted when a user withdraws USDC.
+    /// @param user The address of the user who withdrew USDC.
+    /// @param amount The amount of USDC withdrawn by the user.
+    event Withdrawn(address indexed user, uint256 amount);
+
+    /// @notice Event emitted when the ProjectAllocationManager takes funds from a user.
+    /// @param user The address of the user whose funds were taken.
+    /// @param amount The amount of funds taken from the user.
+    event FundsTaken(address indexed user, uint256 amount);
+
+    /// @notice Event emitted when the ProjectAllocationManager refunds funds to a user.
+    /// @param user The address of the user who was refunded.
+    /// @param amount The amount of funds refunded to the user.
+    event FundsRefunded(address indexed user, uint256 amount);
+
+    /// @notice Event emitted when the vault address is updated.
+    /// @param prevVault The address of the prev vault.
+    /// @param newVault The address of the new vault.
+    event VaultUpdated(address prevVault, address newVault);
+
+    /*//////////////////////////////////////////////////////////////
+                         NON-CONSTANT FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Allows users to deposit USDC into the escrow contract with an option to opt-in or opt-out.
+    /// @dev The deposited amount is added to the user's balance based on their opt-in status.
+    /// @dev Reverts if:
+    /// - the deposit amount is zero.
+    /// - the USDC is not approved to this contract.
+    /// @param amount The amount of USDC to deposit.
+    /// @param optStatus The opt-in status (OptIn or OptOut) for the deposit.
+    function deposit(uint256 amount, TCommon.OptStatus optStatus) external;
+
+    /// @notice Allows users to withdraw their USDC or DynUSDC converted back to USDC based on their opt-in status.
+    /// @dev If the user is opted-in, their DynUSDC is first redeemed for USDC.
+    /// @dev Reverts if:
+    /// - the user has insufficient balance to withdraw.
+    function withdraw() external;
+
+    /*//////////////////////////////////////////////////////////////
+                   NON-CONSTANT ONLY-ADMIN FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
     /// @notice Initializes the Escrow contract by setting the USDC and vault addresses.
     /// @dev Can only be called once. It checks if the contract has already been initialized.
     /// @dev Reverts if:
@@ -12,59 +68,20 @@ interface IEscrow {
     /// @param vaultAddress The address of the Dynavault (yield-bearing vault).
     function initializer(address usdcAddress, address vaultAddress) external;
 
-    /// @notice Allows users to deposit USDC into the escrow contract.
-    /// @dev The deposited amount is added to the user's USDC balance.
-    /// @dev Reverts if:
-    /// - the deposit amount is zero.
-    /// @param amount The amount of USDC to deposit.
-    function deposit(uint256 amount) external;
+    /*//////////////////////////////////////////////////////////////
+                     NON-CONSTANT ONLY-PAM FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
-    /// @notice Allows users to withdraw their USDC, including converting DynUSDC back to USDC if the user is opted in.
-    /// @dev If the user is opted in, their DynUSDC is first redeemed for USDC.
-    /// @dev Reverts if:
-    /// - the user has insufficient USDC to withdraw.
-    /// - the user is opted in and has insufficient DynUSDC to redeem.
-    /// @param usdcAmount The amount of USDC to withdraw.
-    function withdraw(uint256 usdcAmount) external;
+    /// @notice Allows the ProjectAllocationManager to take the user's funds based on their current opt-in status.
+    /// @dev Only the ProjectAllocationManager contract can call this function.
+    /// @param user The address of the user whose funds are being taken.
+    /// @param amount The amount of funds to take from the user.
+    function takeFundsFromUser(address user, uint256 amount) external;
 
-    /// @notice Allows users to opt-in to the Dynavault and convert their USDC to DynUSDC.
-    /// @dev Converts the user's USDC balance into DynUSDC using the Dynavault's deposit mechanism.
-    /// @dev Reverts if:
-    /// - the user has no USDC to convert.
-    /// - the user is already opted into Dynavault.
-    function optIn() external;
-
-    /// @notice Allows users to opt-out of the Dynavault and convert their DynUSDC back to USDC.
-    /// @dev Converts the user's DynUSDC balance back to USDC using the Dynavault's redeem mechanism.
-    /// @dev Reverts if:
-    /// - the user has no DynUSDC to convert.
-    /// - the user is not opted into Dynavault.
-    function optOut() external;
-
-    /// @notice Event emitted when a user deposits USDC.
-    /// @param user The address of the user who deposited USDC.
-    /// @param amount The amount of USDC deposited by the user.
-    event Deposit(address indexed user, uint256 amount);
-
-    /// @notice Event emitted when a user withdraws USDC.
-    /// @param user The address of the user who withdrew USDC.
-    /// @param amount The amount of USDC withdrawn by the user.
-    event Withdraw(address indexed user, uint256 amount);
-
-    /// @notice Event emitted when a user opts in to Dynavault.
-    /// @param user The address of the user who opted into Dynavault.
-    /// @param usdcAmount The amount of USDC that was converted into DynUSDC.
-    /// @param dynUSDCAmount The amount of DynUSDC received by the user.
-    event OptIn(address indexed user, uint256 usdcAmount, uint256 dynUSDCAmount);
-
-    /// @notice Event emitted when a user opts out of Dynavault.
-    /// @param user The address of the user who opted out of Dynavault.
-    /// @param dynUSDCAmount The amount of DynUSDC converted back into USDC.
-    /// @param usdcAmount The amount of USDC received by the user.
-    event OptOut(address indexed user, uint256 dynUSDCAmount, uint256 usdcAmount);
-
-    /// @notice Event emitted when the vault address is updated.
-    /// @param prevVault The address of the prev vault.
-    /// @param newVault The address of the new vault.
-    event VaultUpdated(address prevVault, address newVault);
+    /// @notice Allows the ProjectAllocationManager to refund funds back to the user based on their current opt-in
+    /// status.
+    /// @dev Only the ProjectAllocationManager contract can call this function.
+    /// @param user The address of the user to refund.
+    /// @param amount The amount of funds to refund to the user.
+    function refundFundsToUser(address user, uint256 amount) external;
 }
--- contracts/types/TEscrow.sol
+++ contracts/types/TEscrow.sol
@@ -2,27 +2,20 @@
 
 pragma solidity ^0.8.22;
 
+// types
+import { TCommon } from "./TCommon.sol";
+
 /// @title TEscrow
 /// @notice Library to manage the user struct for the Escrow contract.
 library TEscrow {
-    /// @notice Enum representing whether the user has opted into a yield-bearing vault (Dynavault).
-    /// @dev Tracks the opt-in status for yield accumulation.
-    /// - OptOut: User holds USDC and does not earn yield.
-    /// - OptIn:  User's USDC is converted to DynUSDC, and they earn yield.
-    enum OptStatus {
-        OptOut,
-        OptIn
-    }
-
-    /// @notice Struct to hold user details for the Escrow contract.
-    /// @dev Packed into 3 storage slots
-    /// Total size: 65 bytes (rounded up to 96 bytes due to storage slot alignment).
-    /// @param usdcBalance The balance of USDC held by the user in the Escrow contract. (32 bytes)
-    /// @param dynUSDCBalance The balance of DynUSDC held by the user if they opted into yield earning. (32 bytes)
+    /// @notice Struct to hold user details.
+    ///
+    /// @dev Packed into 2 storage slots
+    /// @param balance The balance held by the user in the Escrow contract.
+    /// Represents USDC if opted-out or DynUSDC if opted-in. (32 bytes)
     /// @param optStatus The current opt-in status of the user, whether they are opted in or out. (1 byte)
     struct UserInfo {
-        uint256 usdcBalance;
-        uint256 dynUSDCBalance;
-        OptStatus optStatus;
+        uint256 balance;
+        TCommon.OptStatus optStatus;
     }
 }
